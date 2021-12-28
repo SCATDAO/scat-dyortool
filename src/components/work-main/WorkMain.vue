07c0acecce273c1470ba15a25bea081e33894aaa
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2021-12-28 15:18:32 -0500

    28-12-2021

--- src/components/work-main/WorkMain.vue
+++ src/components/work-main/WorkMain.vue
@@ -17,7 +17,7 @@
     <div class="css-work-main-banner">
       <div class="css-work-main-bnt">
         <h1>
-          DYOR is a tool for community curation of Dapps in the Cardano
+          Do Your Own Research is a tool for evaluating Dapps in the Cardano
           ecosystem
         </h1>
       </div>
@@ -42,7 +42,26 @@
     <div class="css-work-main-ses">
       <div class="css-work-main-sss">
         <div class="css-work-main-slc">
-          <h2>Knowing your Dapp before investing in it is the first step.</h2>
+          <h2>
+            Designed to teach you the knowledge and resources to properly
+            evaluate a Dapp before investing in it.
+          </h2>
+          <h2>
+            Useful for more experienced investors who to methodically research a
+            project and share the results with others.
+          </h2>
+          <h2>
+            $7.7 billion was stolen in crypto scams in 2021. Do Your Own
+            Research so this doesn’t happen to you.
+          </h2>
+        </div>
+        <div class="css-work-main-slr">
+          <div class="css-work-main-sli">
+            <h2>1. Select a project you want to research.</h2>
+          </div>
+          <div class="css-work-main-sli">
+            <h2>1. Select a project you want to research.</h2>
+          </div>
         </div>
       </div>
     </div>
