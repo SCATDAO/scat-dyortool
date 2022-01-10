5d1b530a81032db8f8f966af227cb72bc2cdcd89
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2022-01-10 17:02:42 -0500

    10-01-2022

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import DyorMain from '../pages/DyorMain.vue'
 import DyorCreate from '../pages/DyorCreate.vue'
 import DyorQuestion from '../pages/DyorQuestion.vue'
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/main' },
         { path: '/main', component: DyorMain },
         { path: '/create', component: DyorCreate },
-        { path: '/research', component: DyorQuestion },
+        { path: '/research', component: DyorQuestion, },
         { path: '/:notFound(.*)', redirect: '/main' }
     ]
 });
